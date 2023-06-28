acc7e7f84cbc089d27efc8268d1313d96906afff
William <104033817+wr-araujo@users.noreply.github.com>
2023-06-28 21:46:47 +0000

    add basic and class folder

---
+++ class/Account.h
@@ -0,0 +1,16 @@
+#include <vector>
+#include <string>
+#include "Transaction.h"
+
+class Account
+{
+private:
+    int balance;
+    std::vector<Transaction> log;
+public:
+    Account();
+    std::vector<std::string> Report();
+    bool Deposit(int amount);
+    bool Withdraw(int amount);
+    int GetBalance() { return balance; }
+};
